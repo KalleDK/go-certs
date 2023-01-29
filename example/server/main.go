0af97e757f3af1379d2b530ea1fa751be8dfec1e
Kalle M. Aagaard <git@k-moeller.dk>
2023-01-29 11:01:28 +0100

    example

--- example/server/main.go
+++ example/server/main.go
@@ -11,27 +11,43 @@ import (
 	"github.com/KalleDK/go-certs"
 )
 
+type DummySwitcher struct {
+	PrimCert string
+	PrimKey  string
+	SecCert  string
+	SecKey   string
+	Store    certs.FileStore
+}
+
+func (ds *DummySwitcher) Switch() {
+	ds.PrimCert, ds.SecCert = ds.SecCert, ds.PrimCert
+	ds.PrimKey, ds.SecKey = ds.SecKey, ds.PrimKey
+	ds.Store.CertPath = ds.PrimCert
+	ds.Store.KeyPath = ds.PrimKey
+}
+
 func serv() {
+
+	// You can see the difference in the certs by the end date
+	// *  expire date: Nov 13 09:45:29 2296 GMT
+	// *  expire date: Aug 29 09:49:43 2570 GMT
+	ds := DummySwitcher{
+		PrimCert: "cert1.pem",
+		PrimKey:  "key1.pem",
+		SecCert:  "cert2.pem",
+		SecKey:   "key2.pem",
+	}
+	ds.Switch()
+
 	mux := http.NewServeMux()
 
-	certmanger := certs.MultiStore{
-		Stores: []certs.Store{
-			&certs.FileStore{
-				CertPath: "cert1.pem",
-				KeyPath:  "cert1.key",
-			},
-			&certs.FileStore{
-				CertPath: "cert2.pem",
-				KeyPath:  "cert2.key",
-			},
-		},
-	}
+	certmanger := &ds.Store
 
 	if err := certmanger.Reload(); err != nil {
 		log.Fatal(err)
 	}
 
-	certs.Notify(&certmanger, syscall.SIGHUP)
+	certs.Notify(certmanger, syscall.SIGHUP)
 
 	tlsConf := &tls.Config{
 		GetCertificate: certmanger.GetCertificate,
@@ -45,7 +61,27 @@ func serv() {
 
 	mux.HandleFunc("/ping", func(w http.ResponseWriter, req *http.Request) {
 		fmt.Fprintf(w, "pong!")
-		certs.Stop(&certmanger)
+	})
+
+	mux.HandleFunc("/disable", func(w http.ResponseWriter, req *http.Request) {
+		certs.Stop(certmanger)
+		fmt.Fprintf(w, "pong!")
+	})
+
+	mux.HandleFunc("/enable", func(w http.ResponseWriter, req *http.Request) {
+		certs.Stop(certmanger)
+		fmt.Fprintf(w, "pong!")
+	})
+
+	mux.HandleFunc("/switch", func(w http.ResponseWriter, req *http.Request) {
+		ds.Switch()
+		fmt.Fprintf(w, "pong!")
+	})
+
+	mux.HandleFunc("/switchreload", func(w http.ResponseWriter, req *http.Request) {
+		ds.Switch()
+		certmanger.Reload()
+		fmt.Fprintf(w, "pong!")
 	})
 
 	srv.ListenAndServeTLS("", "")
